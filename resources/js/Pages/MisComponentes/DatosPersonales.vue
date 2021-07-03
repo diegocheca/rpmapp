3c671e9c3c3d7238e56637167b4927d377da2bb2
Cintia Muñoz <you@example.com>
2021-07-03 00:17:53 -0300

    componentes

--- resources/js/Pages/MisComponentes/DatosPersonales.vue
+++ resources/js/Pages/MisComponentes/DatosPersonales.vue
@@ -1,78 +1,127 @@
 <template>
-    <div class="flex h-screen bg-gray-200 items-center justify-center  mt-32 mb-32">
-        <div class="grid bg-white rounded-lg shadow-xl w-11/12 md:w-9/12 lg:w-1/2"> 
-            <div class="flex justify-center">
-                <div class="flex">
-                    <h1 class="text-gray-600 font-bold md:text-2xl text-xl">Datos Personales</h1>
-                </div>
-            </div>       
 
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7">
-            <div class="grid grid-cols-1">
-                <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Nombre</label>
-                <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Nombre..." />
-            </div>
-
-            <div class="grid grid-cols-1">
-                <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Apellido</label>
-                <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Apellido..." />
-            </div>
-
-            <div class="grid grid-cols-1">
-                <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Tipo de Documento</label> 
-                <select class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
-                    <option>Docuemnto Unico</option>
-                    <option>Pasaporte</option>
-                    <option>CUIT</option>               
-                </select>                 
-            </div>
-
-            <div class="grid grid-cols-1">
-                <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Numero</label>
-                <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Ingrese el número..." />
-            </div>
-
-            <div class="grid grid-cols-1">
-                <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Fecha de Nacimiento</label>
-                <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="date"/>
-            </div>
-
-            <div class="grid grid-cols-1">
-                <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Nacionalidad</label>
-                <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Nacionalidad..." />
-            </div>
-
-            <div class="grid grid-cols-1">
-                <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Profesión</label>
-                <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Profesión..." />
-            </div>
-
-            <div class="grid grid-cols-1">
-                <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Estado Civil</label>
-                <select class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
-                    <option>Casado</option>
-                    <option>Divorcido</option>
-                    <option>Soltero</option>                                  
-                </select>           
-            </div>
-
-            <div class="grid grid-cols-1 mt-5 mx-7"><label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Another Input</label><input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Another Input"></div>
-
+    <!-- Encabezado -->
+    <div class="flex justify-center">
+        <div class="flex">
+            <h1 class="text-gray-900 font-bold md:text-4xl text-xl">Datos Personales</h1>
         </div>
-    
+    </div> 
+        <br>  
+    <div class="grid bg-purple-100 rounded-lg shadow-xl absolute inset-x-20 top-50">         
 
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7">
         
-   
+        <!-- Nombre -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Nombre</label>
+            <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Nombre..." />
+        </div>
 
+        <!-- Apellido -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Apellido</label>
+            <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Apellido..." />
+        </div>
+
+        <!-- Tipo de Documento -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Tipo de Documento</label>
+            <select class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+                <option>DNI</option>
+                <option>Pasaporte</option>
+                <option>CUIT</option>
+            </select>        
+        </div>
+
+        <!-- Numero de DNI -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Número</label>
+            <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Número..." />
+        </div>
+
+        <!-- Fecha de Nacimiento -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Fecha de Nacimiento</label>
+            <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Nombre..." />
+        </div>
+
+        <!-- Nacionalidad -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Nacionalidad</label>
+            <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Nombre..." />
+        </div>
+
+        <!-- Profesión -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Profesión</label>
+            <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Nombre..." />
+        </div>
+
+        <!-- Estado Civil -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Estado Civil</label>
+            <select class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+                <option>Soletero</option>
+                <option>Casado</option>
+                <option>Divorciado</option>
+            </select>        
+        </div>
+
+    </div>
+   
+    <!-- Domicilio Legal -->
+    <div class="grid grid-cols-1 mt-5 mx-7">
+      <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Domicilio Legal</label>
+      <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Domicilio Legal" />
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7">
+        
+        <!-- Provincia Domicilio Legal -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Provincia</label>
+            <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Provincia Domicilio Legal..." />
+        </div>
+
+        <!-- Departamento Domicilio Legal -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Departamento</label>
+            <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Dpto Domicilio Legal..." />
+        </div>
+
+    </div>
+
+    <!-- Domicilio Real -->
+    <div class="grid grid-cols-1 mt-5 mx-7">
+      <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Domicilio Real</label>
+      <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Domicilio Real..." />
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7">
+        
+        <!-- Provincia Domicilio Real -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Provincia</label>
+            <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Provincia Domicilio Real..." />
+        </div>
+
+        <!-- Departamento Domicilio Real -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Departamento</label>
+            <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Dpto Domicilio Real..." />
+        </div>
+
+    </div>
+    <br>
+    <!-- Botones -->
     <div class='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
-      <button class='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Cancel</button>
-      <button class='w-auto bg-purple-500 hover:bg-purple-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Create</button>
+      <button class="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-gray-700 hover:bg-gray-900 hover:shadow-lg">Cancelar</button>
+      <button class="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-blue-500 hover:bg-blue-600 hover:shadow-lg">Guardar</button>
     </div>
 
   </div>
-</div>
-    
 </template>
+
 <script>
 export default {
     setup() {
--- resources/js/Pages/formWeb/formPrueba.vue
+++ resources/js/Pages/formWeb/formPrueba.vue
@@ -1,19 +1,45 @@
 <template>
     <app-layout>
+        <!--Winzard-->
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                     <Mostrar></Mostrar>
-                </div>
-            </div>
+                </div>                                   
+            </div>                   
+        </div> 
+
+        <!--Datos Personales-->
+        <div >
+            <DatosPersonales></DatosPersonales> 
+        </div> 
+
+        <!--Datos Representante Legal-->
+        <div >
+            <DatosRepresentanteLegal></DatosRepresentanteLegal> 
         </div>
+
+        <!--Ubicacion Solicitud de Exploracion-->
+        <div >
+            <UbicacionSolicitudExploracion></UbicacionSolicitudExploracion> 
+        </div>           
     </app-layout>
+    
+
+    
+
 </template>
 <script>
-import Mostrar from "./componentes/wizard.vue";
+import Mostrar from "./componentes/wizard.vue";  
+import DatosPersonales from "../MisComponentes/DatosPersonales.vue";
+// import DatosRepresentanteLegal from "../MisComponentes/DatosRepresentanteLegal.vue";
+//import UbicacionSolicitudExploracion from "../MisComponentes/UbicacionSolicitudExploracion.vue";
 export default {
     components: {
         Mostrar,
+        DatosPersonales,
+      //  DatosRepresentanteLegal,
+   // UbicacionSolicitudExploracion
     },
     props: {
         errors: Object,
