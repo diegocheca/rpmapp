2ffb34d95b0589e21bb4ad2a66ef0c1da46cb640
Cintia Muñoz <you@example.com>
2021-07-08 20:21:42 -0300

    componentes nuevos

---
+++ resources/js/Pages/formWeb/componentes/DatosRepresentanteLegal.vue
@@ -0,0 +1,75 @@
+<template>
+        <!-- Encabezado -->
+        <div class="flex justify-center">
+            <div class="flex">
+                <h1 class="text-gray-600 font-bold md:text-2xl text-xl">Datos Representante Legal</h1>
+            </div>
+        </div>
+        <br>
+    <div class="grid bg-purple-100 rounded-lg shadow-xl absolute inset-x-20 top-50 ">   
+
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7">        
+        <!-- Nombre -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Nombre</label>
+            <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Nombre..." />
+        </div>
+
+        <!-- Apellido -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Apellido</label>
+            <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Apellido..." />
+        </div>
+
+        <!-- Tipo de Documento -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Tipo de Documento</label>
+            <select class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+                <option>DNI</option>
+                <option>Pasaporte</option>
+                <option>CUIT</option>
+            </select>        
+        </div>
+
+        <!-- Numero de DNI -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Número</label>
+            <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Número..." />
+        </div> 
+    </div>
+   
+        <!-- Domicilio  -->
+        <div class="grid grid-cols-1 mt-5 mx-7">
+        <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Domicilio </label>
+        <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Domicilio..." />
+        </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7">        
+        <!-- Provincia -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Provincia</label>
+            <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Provincia ..." />
+        </div>
+
+        <!-- Departamento -->
+        <div class="grid grid-cols-1">
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Departamento</label>
+            <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" placeholder="Departamento..." />
+        </div>
+    </div>      
+    
+        <!-- Botones -->
+        <div class='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
+            <button class="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-gray-700 hover:bg-gray-900 hover:shadow-lg">Cancelar</button>
+            <button class="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-blue-500 hover:bg-blue-600 hover:shadow-lg">Guardar</button>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
--- resources/js/Pages/formWeb/formPrueba.vue
+++ resources/js/Pages/formWeb/formPrueba.vue
@@ -22,24 +22,74 @@
         <!--Ubicacion Solicitud de Exploracion-->
         <div >
             <UbicacionSolicitudExploracion></UbicacionSolicitudExploracion> 
-        </div>           
+        </div> 
+
+        <!--Matricula Catastral-->
+        <div >
+            <MatriculaCatastral></MatriculaCatastral> 
+        </div> 
+
+         <!--Cordenadas GaussKruger-->
+        <div >
+            <CordenadasGaussKruger></CordenadasGaussKruger> 
+        </div> 
+
+        <!--Categoria Mineral-->
+        <div >
+            <CategoriaMineral></CategoriaMineral> 
+        </div>    
+
+        <!--Datos Propietario-->
+        <div >
+            <DatosPropietario></DatosPropietario> 
+        </div> 
+
+        <!--Cantidad de Permisos-->
+        <div >
+            <CantidadPermisos></CantidadPermisos> 
+        </div>  
+
+        <!--Declaracion Jurada-->
+        <div >
+            <DeclaracionJurada></DeclaracionJurada> 
+        </div> 
+
+        <!--Informe Registro Catastral-->
+        <div >
+            <InformeRegistroCatastral></InformeRegistroCatastral> 
+        </div>       
     </app-layout>
     
 
     
 
 </template>
+
 <script>
 import Mostrar from "./componentes/wizard.vue";  
-import DatosPersonales from "../MisComponentes/DatosPersonales.vue";
-// import DatosRepresentanteLegal from "../MisComponentes/DatosRepresentanteLegal.vue";
-//import UbicacionSolicitudExploracion from "../MisComponentes/UbicacionSolicitudExploracion.vue";
+// import DatosPersonales from "./componentes/DatosPersonales.vue";
+// import DatosRepresentanteLegal from "./componentes/DatosRepresentanteLegal.vue";
+// import UbicacionSolicitudExploracion from "./componentes/UbicacionSolicitudExploracion.vue";
+// import MatriculaCatastral from "./componentes/MatriculaCatastral.vue";
+// import CordenadasGaussKruger from "./componentes/CordenadasGaussKruger.vue";
+// import  CategoriaMineral from "./componentes/CategoriaMineral.vue";
+// import  DatosPropietario from "./componentes/DatosPropietario.vue";
+// import  CantidadPermisos from "./componentes/CantidadPermisos.vue";
+// import  DeclaracionJurada from "./componentes/DeclaracionJurada.vue";
+   import  InformeRegistroCatastral from "./componentes/InformeRegistroCatastral.vue";   
 export default {
     components: {
         Mostrar,
-        DatosPersonales,
-      //  DatosRepresentanteLegal,
-   // UbicacionSolicitudExploracion
+        // DatosPersonales,
+        // DatosRepresentanteLegal,
+        // UbicacionSolicitudExploracion,
+        // MatriculaCatastral,      
+        // CordenadasGaussKruger,
+        // CategoriaMineral,
+        // DatosPropietario,
+        //CantidadPermisos,
+        //DeclaracionJurada,
+        InformeRegistroCatastral,
     },
     props: {
         errors: Object,
