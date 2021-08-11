7e3fbd2f9aec88ec19e4736340f8a7c41d1ea6c7
Luis Torres <ltorres.godoy77@gmail.com>
2021-08-11 17:38:48 -0300

    formWeb

--- routes/web/web.php
+++ routes/web/web.php
@@ -18,11 +18,6 @@ use App\Http\Controllers\ProductoresController;
 use App\Http\Controllers\CountriesController;
 use App\Http\Controllers\UsersController;
 
-/************* FORMULARIOS WEB *************/
-
-//use App\Http\Controllers\SolicitudesController;
-use App\Http\Controllers\formWebController\SolicitudesController;
-use App\Http\Controllers\formWebController\TipoDocumentoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -194,22 +189,3 @@ Route::group(['prefix' => 'paises'], function () {
         ->middleware(['auth:sanctum', 'verified']);
 });
 
-//FORMULARIOS WEB
-// Route::group(['prefix' => 'solicitudes'], function () {
-Route::resource('solicitudes', SolicitudesController::class)
-    ->middleware(['auth:sanctum', 'verified'])
-    ->names('solicitudes');
-
-Route::post('solicitudes/solicitudesDatos', [SolicitudesController::class, 'postSolicitudes'])
-    ->middleware(['auth:sanctum', 'verified']);
-// Route::get('prueba', 'SolicitudesController@prueba')
-//     ->middleware(['auth:sanctum', 'verified']);
-
-// Route::get('/prueba', [SolicitudesController::class, "prueba"])->name('solicitudes.joanna');
-Route::get('/prueba', [SolicitudesController::class, "prueba"])->name('prueba');
-
-// });
-Route::get('/menu', [SolicitudesController::class, "menu"])->name('menu');
-
-Route::get('tipo_documento', [TipoDocumentoController::class, "getTipoDocumento"])
-    ->middleware(['auth:sanctum', 'verified']);
