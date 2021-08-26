f5793acdf958273919b99ea687245d2a2cd40df6
diegoc <diegochecarelli@gmail.com>
2021-08-25 22:34:18 -0300

    avances de Luis

--- routes/web/web.php
+++ routes/web/web.php
@@ -137,7 +137,9 @@ Route::post('/formularios/validar_mina_para_prod', [FormAltaProductorController:
 
 Route::get('/gracias_confirmacion/{codigo}', [FormAltaProductorController::class, "validar_email_desde_email"])->name('validar-email-desde-email');
 
+
 //evaluacion de formularios presentados
+
 
 Route::post('/formularios/evaluacion_auto_guardado_uno', [FormAltaProductorController::class, "correccion_guardar_paso_uno"])->name('correccion_guardar-paso-uno');
 Route::post('/formularios/evaluacion_auto_guardado_dos', [FormAltaProductorController::class, "correccion_guardar_paso_dos"])->name('correccion_guardar-paso-dos');
