b28feab6b94ad199407a5bb03b8008a8a9693301
Luis Torres <ltorres.godoy77@gmail.com>
2021-09-20 20:19:27 -0300

    actualizacion kucho

--- routes/web/formWeb.php
+++ routes/web/formWeb.php
@@ -24,6 +24,7 @@ use App\Http\Controllers\formWebController\SolicitudesController;
 use App\Http\Controllers\formWebController\TipoDocumentoController;
 use App\Http\Controllers\formWebController\EstadoTerrenoController;
 use App\Http\Controllers\formWebController\MineralController;
+use App\Http\Controllers\formWebController\EstadoSolicitudController;
 
 
 /*
@@ -37,14 +38,14 @@ use App\Http\Controllers\formWebController\MineralController;
 |
 */
 
-Route::resource('solicitudes', SolicitudesController::class) 
-    ->middleware(['auth:sanctum', 'verified']);
+Route::resource('solicitudes', SolicitudesController::class)
+        ->middleware(['auth:sanctum', 'verified']);
 
- // Route::post('solicitudes/create', [SolicitudesController::class,'postSolicitudes']) //va a postSolicitudes
- //       ->middleware(['auth:sanctum', 'verified']);
+// Route::post('solicitudes/create', [SolicitudesController::class,'postSolicitudes']) //va a postSolicitudes
+//       ->middleware(['auth:sanctum', 'verified']);
 
-Route::post('solicitudes/solicitudesDatos', [SolicitudesController::class,'postSolicitudes']) //procesa datos solicitud exploracion
-  ->middleware(['auth:sanctum', 'verified']);
+Route::post('solicitudes/solicitudesDatos', [SolicitudesController::class, 'postSolicitudes']) //procesa datos solicitud exploracion
+        ->middleware(['auth:sanctum', 'verified']);
 
 Route::get('tipo_documento', [TipoDocumentoController::class, "getTipoDocumento"])
         ->middleware(['auth:sanctum', 'verified']);
@@ -55,11 +56,19 @@ Route::get('mineral', [MineralController::class, "getMineral"])
 Route::get('estado_terreno', [EstadoTerrenoController::class, "getEstadoTerreno"])
         ->middleware(['auth:sanctum', 'verified']);
 
+Route::get('estado_solicitud', [EstadoSolicitudController::class, "getEstadoSolicitud"])
+        ->middleware(['auth:sanctum', 'verified']);
+
 Route::middleware(['auth:sanctum', 'verified'])
-        ->get('/solicitudes.descubrimiento',[SolicitudesController::class,"create2"])//create para solicitud descubrimiento
-        ->name('descubrimiento');      
+        ->get('/solicitudes.descubrimiento', [SolicitudesController::class, "create2"]) //create para solicitud descubrimiento
+        ->name('descubrimiento');
 
+Route::middleware(['auth:sanctum', 'verified'])
+        ->get('/solicitudes.descubrimiento', [SolicitudesController::class, "crear_descu"]) //create para solicitud descubrimiento
+        ->name('descubrimiento');
 
-Route::post('solicitudesProcesa', [SolicitudesController::class,"procesa"])//procesa datos solicitud descubrimiento
-        ->middleware(['auth:sanctum', 'verified']);  
+Route::post('solicitudesProcesa', [SolicitudesController::class, "procesa"]) //procesa datos solicitud descubrimiento
+        ->middleware(['auth:sanctum', 'verified']);
 
+Route::post('solicitudes/solicitudesProcesa', [SolicitudesController::class, 'postSolicitudes']) //procesa datos solicitud descubrimiento
+        ->middleware(['auth:sanctum', 'verified']);
