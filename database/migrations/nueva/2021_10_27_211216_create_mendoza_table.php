5e89cf8b6d472ef34da1950d7d4596099c5c9b1f
diegocheca <diegochecarelli@gmail.com>
2021-10-27 20:22:15 -0300

    creadno migration para tabla mendoza

---
+++ database/migrations/nueva/2021_10_27_211216_create_mendoza_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMendozaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('form_alta_productoresMendoza', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('id_formulario_alta');
+            $table->string('decreto3737', 100)->default(null)->nullable();
+            $table->boolean('decreto3737_correcto')->default(null)->nullable();
+            $table->string('obs_gestor_nombre_apellido', 200)->default(null)->nullable();
+            
+            $table->string('situacion_mina', 100)->default(null)->nullable();
+            $table->boolean('situacion_mina_correcto')->default(null)->nullable();
+            $table->string('obs_situacion_mina', 200)->default(null)->nullable();
+
+            $table->string('concesion_minera_asiento_n', 100)->default(null)->nullable();
+            $table->integer('concesion_minera_fojas')->default(null)->nullable();
+            $table->integer('concesion_minera_tomo_n')->default(null)->nullable();
+            $table->string('concesion_minera_reg_m_y_d', 100)->default(null)->nullable();
+            $table->string('concesion_minera_reg_cant', 100)->default(null)->nullable();
+            $table->string('concesion_minera_reg_men', 100)->default(null)->nullable();
+            $table->string('concesion_minera_reg_d_y_c', 100)->default(null)->nullable();
+            
+            $table->integer('created_by')->default(null)->nullable();
+            $table->integer('updated_by')->default(null)->nullable();
+            $table->timestamps();
+            $table->softDeletes($column = 'deleted_at');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('form_alta_productoresMendoza');
+    }
+}
