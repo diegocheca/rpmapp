0633d13484b6937080cafb74fd2a8457c132ecde
Rodrigo Kokot <rodrigokokot@gmail.com>
2022-07-01 17:21:47 -0300

    commit salta

--- app/Http/Controllers/ReinscripcionController.php
+++ app/Http/Controllers/ReinscripcionController.php
@@ -38,7 +38,8 @@ class ReinscripcionController extends Controller
         'RíoNegro',
         'Neuquén',
         'SantiagodelEstero',
-        'Tucumán'
+        'Tucumán',
+        'Salta'
     ];
 
     /**
