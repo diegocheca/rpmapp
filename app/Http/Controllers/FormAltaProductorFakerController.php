9753bb4617cea45ff9121cd6fd524ae1272068a3
diegocheca <diegochecarelli@gmail.com>
2022-09-07 20:28:13 -0300

    visual de seeder

--- app/Http/Controllers/FormAltaProductorFakerController.php
+++ app/Http/Controllers/FormAltaProductorFakerController.php
@@ -6,6 +6,26 @@ use App\Http\Requests\StoreFormAltaProductorFakerRequest;
 use App\Http\Requests\UpdateFormAltaProductorFakerRequest;
 use App\Models\FormAltaProductorFaker;
 use Inertia\Inertia;
+use App\Models\Provincias;
+
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Factory as Faker;
+use App\Models\FormAltaProdPaso1;
+use App\Models\FormularioAltaProd;
+use App\Models\FormAltaProductor; //el q tiene mil columnas
+
+use Carbon\Carbon;
+use App\Models\Departamentos;
+use App\Models\User;
+use App\Models\Minerales;
+use App\Models\Constants;
+use stdClass;
+use App\Models\FormAltaProductorCatamarca;
+
+use App\Models\FormAltaProductorMendoza;
+use App\Models\FormAltaProductorSalta;
 
 class FormAltaProductorFakerController extends Controller
 {
@@ -17,9 +37,101 @@ class FormAltaProductorFakerController extends Controller
     public function index()
     {
         //
+        $provincias = Provincias::select('id','nombre')->get();
         return Inertia::render('Fakers/FormularioALtaProductor/index', [
-            'number' => 1,
+            'provincias' => $provincias,
         ]);
+    }
+
+    public function create_formularios_alta_productores(Request $request){
+        //comprobar cuantos quiero crear y si el request llega bien
+        $faker = Faker::create();
+        $provincias = Provincias::select("id", "nombre")->get()->toArray();
+        $array_to_return = array();
+        for($i = 0;$i< 2; $i++)
+        {
+            //INICIO DE VARIABLES
+            $razon_social = $faker->name();
+            $email = str_replace(" ","",$razon_social)."@gmail.com";
+            $num_aleatorio_provincia = $faker->numberBetween(0,count($provincias)-1);
+            $id_provincia = $provincias[$num_aleatorio_provincia]["id"];
+            $nombre_provincia = $provincias[$num_aleatorio_provincia]["nombre"];
+            $departamento = Departamentos::select("id", "nombre")->where("provincia_id", "=", $num_aleatorio_provincia)->first();
+            if($departamento == null ){
+                $departamento = new stdClass();
+                $departamento->id = 9999;
+                $departamento->nombre ="departamento";
+            }
+            if(!property_exists($departamento, 'id') ){
+                $departamento->id = 9999;
+                $departamento->nombre ="departamento";
+            }
+            $sociedad = Constants::$sociedades[$faker->numberBetween(0,count(Constants::$sociedades)-1)];
+            $id_user = 0;
+			//crear el usuario
+			$resultado = User::create([
+				'name' => $razon_social,
+				'email' => $email,
+				'password' => bcrypt('password'),
+				'current_team_id' => 10, // team_catamarca
+				'profile_photo_path' => "profile-photos/catamarca.png",
+				'first_name' => $razon_social,
+				'last_name' =>  "nada",
+				'provincia' => $nombre_provincia,
+				'id_provincia' => $id_provincia,
+			])->assignRole('Productor');
+			$id_user = $resultado->id;
+			//Termino de crear usuario
+
+            /*Crear formulario*/
+            //$estado = Constants::$estados[$faker->numberBetween(0,count(Constants::$estados)-1)]; 
+            $nuevo_formulario = new FormAltaProductor();
+
+            $formulario_nuevo  = new FormAltaProductor();
+            //$formulario_nuevo->inicializar_faker(null,$id_user,$id_provincia);
+            $formulario_nuevo->completar_paso1_faker(null,null,$razon_social,$email,$sociedad,null,null,null,$id_user,$id_provincia);
+            $formulario_nuevo->aprobar_paso_uno($id_user);
+            
+            $formulario_nuevo->completar_paso2_faker(null,null,null,$id_provincia,null,null,null,null,$id_user);
+            $formulario_nuevo->aprobar_paso_dos($id_user);
+
+            $formulario_nuevo->completar_paso3_faker(null,null,null,$id_provincia,null,null,null,null,$id_user);
+            $formulario_nuevo->aprobar_paso_tres($id_user);
+            
+            $formulario_nuevo->completar_paso4_faker(null,null,null,null,null,null,null,$id_user);
+            $formulario_nuevo->aprobar_paso_cuatro($id_user);
+
+            $formulario_nuevo->completar_paso5_faker(null, null, null, null, null,$id_user);
+            $formulario_nuevo->aprobar_paso_cinco($id_user);
+
+            $formulario_nuevo->completar_paso6_faker($id_provincia,$departamento->id,$id_user);
+            $formulario_nuevo->aprobar_paso_seis($id_user);
+            
+            if($id_provincia == 10){//si es catamarca
+                $formulario_catamarca = new FormAltaProductorCatamarca();
+                $formulario_catamarca->completar_paso_catamarca_faker(null, null,null,null, null, null, null, null, null, null, null,$formulario_nuevo->id,$id_user);
+                $formulario_catamarca->aprobar_paso_catamarca($id_user);
+            }
+            if($id_provincia == 50){//mendoza poner el correcto de mendoza
+                $formulario_mendoza = new FormAltaProductorMendoza();
+                $formulario_mendoza->completar_y_guardar_formu_fake($formulario_nuevo->id,$id_user);
+                $formulario_catamarca->aprobar_paso_mendoza($id_user);
+            }
+            if($id_provincia == 66){//mendoza poner el correcto de salta
+                $formulario_mendoza = new FormAltaProductorSalta();
+                $formulario_mendoza->crear_formulario_fake_salta($formulario_nuevo->id,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null, $id_user);
+            }
+            $formulario_nuevo->completar_paso7_faker(null,null,null,null,$id_user);
+            $formulario_nuevo->completar_paso8_faker($id_user);
+            $array_to_return[$i] = [
+                "id_fomulario" => $formulario_nuevo->id,
+                "id_usario" => $id_user->id,
+                "email" => $email,
+                "provincia" => $id_provincia
+
+            ];
+        }
+
     }
 
     /**
---
+++ resources/js/Pages/Fakers/FormularioALtaProductor/index.vue
@@ -0,0 +1,354 @@
+<style scoped>
+  body {
+    background-color: #dfe4e9;
+  }
+  
+  .stage {
+    overflow: hidden;
+    position: absolute;
+    top: 20%;
+    left: 15%;
+    margin: -175px 0 0 -250px;
+    width: 500px;
+    height: 350px;
+  }
+  
+  .factory {
+    position: absolute;
+    width: 100px;
+    height: 200px;
+    background: #e6c747;
+    transform: translate(50px, 100px);
+  }
+  .factory::before {
+    background: #c6a41b;
+    border-radius: 1px;
+    width: 110px;
+    height: 10px;
+    top: -10px;
+    left: -5px;
+  }
+  .factory::after {
+    width: 20px;
+    height: 20px;
+    top: 10px;
+    left: 10px;
+    border-radius: 50%;
+    background-color: #c96160;
+  }
+  
+  .magic {
+    background: #b8e3f0;
+    width: 40px;
+    height: 45px;
+    position: absolute;
+    border-radius: 0 0 3px 3px;
+    transform: translate(0, -60px);
+  }
+  .magic::before {
+    background: #af9218;
+    bottom: -5px;
+    left: 20%;
+    width: 60%;
+    height: 5px;
+  }
+  .magic::after {
+    background: #27af61;
+    border-radius: 0 0 3px 3px;
+    bottom: 3px;
+    left: 3px;
+    width: 34px;
+    height: 70%;
+    animation: upDown 9000ms linear infinite normal;
+  }
+  
+  .screen {
+    width: 60px;
+    height: 50px;
+    background-color: #415060;
+    position: absolute;
+    border-radius: 5px;
+    border: 3px solid #fbf7e4;
+    transform: translate(15px, 60px);
+    overflow: hidden;
+  }
+  .screen::before, .screen::after {
+    top: 0;
+    left: 0;
+    width: 40px;
+    height: 40px;
+    box-shadow: inset 0 0 0 1px rgba(76, 215, 135, 0.2);
+    background-color: rgba(76, 215, 135, 0.1);
+    transform: translate(-100px, 0px);
+    animation: moveRightPseudo 650ms linear infinite normal;
+  }
+  
+  .band {
+    position: absolute;
+    width: 350px;
+    height: 15px;
+    background: #364451;
+    border-radius: 8px;
+    transform: translate(50px, 210px);
+  }
+  .band::before {
+    background-image: radial-gradient(#4b5d6f 40%, rgba(0, 0, 0, 0) 41%);
+    background-size: 15px 15px;
+    background-position: 0px 0px;
+    width: 90%;
+    height: 15px;
+    top: 0px;
+    right: 5px;
+    z-index: 2;
+    animation: band 2150ms linear infinite normal;
+  }
+  .band::after {
+    background: #4b5d6f;
+    width: 79%;
+    height: 75px;
+    top: 15px;
+    left: 57px;
+    transform: skew(-15deg);
+  }
+  
+  ul {
+    position: absolute;
+    margin: 0;
+    padding: 0;
+    width: 420px;
+    height: 140px;
+    overflow: hidden;
+    transform: translate(50px, 160px);
+  }
+  ul:after {
+    background: #415060;
+    border-radius: 50%;
+    width: 60px;
+    height: 5px;
+    bottom: 0px;
+    right: 20px;
+    z-index: 0;
+  }
+  
+  li {
+    overflow: hidden;
+    z-index: 1;
+    position: absolute;
+    width: 40px;
+    height: 40px;
+    background-color: #cf7473;
+    transform: translate(0px, 10px);
+    transform-origin: center bottom;
+    animation-name: moveRight;
+    animation-duration: 3000ms;
+    animation-timing-function: linear;
+    animation-direction: normal;
+    animation-iteration-count: infinite;
+  }
+  li::after {
+    dsplay: block;
+    width: 25px;
+    height: 7px;
+    background-color: #c34e4d;
+    top: 5px;
+    left: 5px;
+  }
+  
+  li:nth-child(1) {
+    animation-delay: 0ms;
+  }
+  
+  li:nth-child(2) {
+    animation-delay: 600ms;
+  }
+  
+  li:nth-child(3) {
+    animation-delay: 1200ms;
+  }
+  
+  li:nth-child(4) {
+    animation-delay: 1800ms;
+  }
+  
+  li:nth-child(5) {
+    animation-delay: 2400ms;
+  }
+  
+  .ps, .factory::before, .factory::after, .magic::before, .magic::after, .screen::before, .screen::after, .band::before, .band::after, ul:after, li::after {
+    content: '';
+    position: absolute;
+  }
+  
+  @keyframes moveRight {
+    0% {
+      transform: translate(0px, 10px);
+    }
+    80% {
+      transform: translate(325px, 10px);
+    }
+    90% {
+      transform: translate(330px, 13px) rotateZ(90deg);
+    }
+    100% {
+      transform: translate(340px, 110px) rotateZ(135deg);
+    }
+  }
+  @keyframes moveRightPseudo {
+    0% {
+      transform: translate(-50px, 6px);
+    }
+    100% {
+      transform: translate(60px, 6px);
+    }
+  }
+  @keyframes upDown {
+    0% {
+      height: 70%;
+    }
+    70% {
+      height: 5%;
+    }
+    100% {
+      height: 70%;
+    }
+  }
+  @keyframes band {
+    0% {
+      background-position: 0px 0px;
+    }
+    100% {
+      background-position: 100% 0px;
+    }
+  }
+  
+  </style>
+  <template>
+    <app-layout>
+     <div class="container px-6 mx-auto">
+        <div
+          class="flex flex-col text-center md:text-left md:flex-row h-screen justify-evenly md:items-center"
+        >
+          <div class="flex flex-col w-full">
+            <div class="w-full md:w-6/6 lg:w-6/6 mx-auto md:mx-0">
+                <div class='stage'>
+                <ul>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <div class='band'></div>
+                <div class='factory'>
+                  <div class='magic'></div>
+                  <div class='screen'></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="w-full md:w-6/6 lg:w-6/6 mx-auto md:mx-0">
+            <div class="bg-white p-10 flex flex-col w-full shadow-xl rounded-xl">
+              <h2 class="text-2xl font-bold text-gray-800 text-left mb-5">
+                Creating formularios alta prod faked
+              </h2>
+              <form action="" class="w-full">
+                <div id="input" class="flex flex-col w-full my-5">
+                  <label for="username" class="text-gray-500 mb-2"
+                    >How many?</label
+                  >
+                  <input
+                    type="number"
+                    id="username"
+                    placeholder="10"
+                    class="appearance-none border-2 border-gray-100 rounded-lg px-4 py-3 placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600 focus:shadow-lg"
+                  />
+                </div>
+                <div id="input" class="flex flex-col w-full my-5">
+                  <label for="username" class="text-gray-500 mb-2"
+                    >Provincia</label
+                  >
+                  <select>
+                    <option value="-1" selected>Cualquiera</option>
+                    <option v-for="provincia in provincias" v-bind:key="provincia.id" :value="provincia.id">
+                                        {{provincia.nombre}}
+                                    </option>
+                  </select>
+                  
+                </div>
+                <div id="button" class="flex flex-col w-full my-5">
+                  <button
+                    type="button"
+                    class="w-full py-4 bg-green-600 rounded-lg text-green-100"
+                  >
+                    <div class="flex flex-row items-center justify-center">
+                      <div class="mr-2">
+                        <svg
+                          class="w-6 h-6"
+                          fill="none"
+                          stroke="currentColor"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+                          ></path>
+                        </svg>
+                      </div>
+                      <div class="font-bold">Sigin</div>
+                    </div>
+                  </button>
+                  <div class="flex justify-evenly mt-5">
+                    <a
+                      href="#"
+                      class="w-full text-center font-medium text-gray-500"
+                      >Recover password!</a
+                    >
+                    <a
+                      href="#"
+                      class="w-full text-center font-medium text-gray-500"
+                      >Singup!</a
+                    >
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </app-layout>
+  </template>
+  
+  
+  <script>
+  import AppLayout from "@/Layouts/AppLayout";
+  import JetDialogModal from '@/Jetstream/DialogModal';
+  //import Button from '../../Jetstream/Button.vue';
+  
+  export default {
+    props: {
+      provincias: Object,
+    },
+    
+    data() {
+      return {
+        confirmingUserDeletion:false,
+        modal_tittle: 'este es el titulo',
+        modal_body: 'este es el cuerpo',
+      };
+    },
+    components: {
+      AppLayout,
+      JetDialogModal
+      //Button,
+    },
+    methods: {
+      closeModal() {
+          this.confirmingUserDeletion = false
+          //this.form.reset()
+      },
+    }
+  };
+  </script>
