fb093fd9a20b0c009185bdbc3ba0272399d7a713
diegocheca <diegochecarelli@gmail.com>
2022-09-12 09:30:13 -0300

    before migrations ui

--- database/migrations/2021_06_25_214254_create_minerales__borradores_table.php
+++ database/migrations/2021_06_25_214254_create_minerales__borradores_table.php
@@ -14,8 +14,34 @@ class CreateMineralesBorradoresTable extends Migration
     public function up()
     {
         Schema::create('minerales__borradores', function (Blueprint $table) {
-            $table->id();
+
+            $table->bigIncrements('id_formulario');
+            $table->in('id_mineral')->nullable()->default(null);
+            $table->string('lugar_donde_se_encuentra', 200)->default(null);
+            $table->string('variedad', 100)->default(null);
+            $table->string('segunda_cat_mineral_explotado', 200)->default(null);
+            $table->string('mostrar_lugar_segunda_cat', 100)->default(null);
+            $table->string('mostrar_otro_mineral_segunda_cat', 100)->default(null);
+            $table->string('otro_mineral_segunda_cat', 100)->default(null);
+            $table->string('observacion', 100)->default(null);
+            $table->string('clase_text_area_presentacion', 100)->default(null);
+            $table->string('clase_text_evaluacion_de_text_area_presentacion', 100)->default(null);
+            $table->string('texto_validacion_text_area_presentacion', 100)->default(null);
+            $table->string('presentacion_valida', 100)->default(null);
+            $table->string('evaluacion_correcto', 100)->default(null);
+            $table->string('observacion_autoridad', 100)->default(null);
+            $table->string('clase_text_area', 100)->default(null);
+            $table->string('clase_text_evaluacion_de_text_area', 100)->default(null);
+            $table->string('texto_validacion_text_area', 100)->default(null);
+            $table->text('obs_valida')->default(null);
+            $table->text('lista_de_minerales_array')->default(null);
+            $table->string('thumb', 256)->default(null);
+            $table->integer('created_by');
+            $table->integer('updated_by');
+            $table->string('estado', 100)->default('sin revisar');
+
             $table->timestamps();
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
         });
     }
 
