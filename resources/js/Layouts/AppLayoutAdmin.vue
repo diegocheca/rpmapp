4d1b8b8a61c9515a3f4a2db351dd54a735e3b887
diegocheca <diegochecarelli@gmail.com>
2022-09-12 14:24:29 -0300

    before migrations ui added composer en menu

--- resources/js/Layouts/AppLayoutAdmin.vue
+++ resources/js/Layouts/AppLayoutAdmin.vue
@@ -343,6 +343,32 @@
             </svg>
               <span class="mx-3">Faker Form Alt</span>
             </inertia-link>
+            <inertia-link
+              class="
+                flex
+                items-center
+                mt-4
+                py-2
+                px-6
+                text-gray-500
+                hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100
+                border-l-4 border-transparent
+                hover:border-indigo-500
+              "
+              :href="route('admin.migrations')"
+              v-on:click="activetab = 'Migrations'"
+              v-bind:class="[
+                activetab === 'Migrations'
+                  ? 'active border-indigo-500 text-gray-100 bg-gray-700 bg-opacity-25 font-black'
+                  : '',
+              ]"
+            >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 14.25h13.5m-13.5 0a3 3 0 01-3-3m3 3a3 3 0 100 6h13.5a3 3 0 100-6m-16.5-3a3 3 0 013-3h13.5a3 3 0 013 3m-19.5 0a4.5 4.5 0 01.9-2.7L5.737 5.1a3.375 3.375 0 012.7-1.35h7.126c1.062 0 2.062.5 2.7 1.35l2.587 3.45a4.5 4.5 0 01.9 2.7m0 0a3 3 0 01-3 3m0 3h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008zm-3 6h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008z" />
+            </svg>
+
+              <span class="mx-3">Migrations</span>
+            </inertia-link>
           </nav>
         </div>
       </div>
