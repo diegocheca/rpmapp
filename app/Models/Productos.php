ca4c06f640c3d2a68d7eaf7c006e1c4756ea0cb7
diegocheca <diegochecarelli@gmail.com>
2022-09-16 16:48:06 -0300

    seeders productos

--- app/Models/Productos.php
+++ app/Models/Productos.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Faker\Factory as Faker;
 
+use App\Models\Minerales_Borradores;
+
 class Productos extends Model
 {
     use HasFactory;
@@ -82,9 +84,12 @@ class Productos extends Model
     public function producto_faker($id_reinscripcion,$user_id){
         $faker = Faker::create();
 
+        $minerales = Minerales_Borradores::all();
+        $mineral = $minerales[$faker->numberBetween(0,count($minerales))];
+
         $this->id_reinscripcion = $id_reinscripcion;
         $this->id_mina = null;
-        $this->nombre_mineral = null;
+        $this->nombre_mineral = $mineral->id;
         $this->variedad = "natural";
         $this->produccion = $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 999999);
         $this->unidades = "ton";
@@ -128,6 +133,74 @@ class Productos extends Model
 
         $this->save();
 
+        // San Luis
+        /*'ley',
+        'calidad',
+        'observaciones',*/
+    }
+
+
+    public function producto_evaluacion_faker($user_id,$aprobado){
+        $faker = Faker::create();
+
+        $minerales = Minerales_Borradores::all();
+        $mineral = $minerales[$faker->numberBetween(0,count($minerales))];
+        if($aprobado){
+            $this->nombre_mineral_evaluacion = null;
+            $this->id_mina_evaluacion = null;
+            $this->variedad_evaluacion = null;
+            $this->produccion_evaluacion = null;
+            $this->unidades_evaluacion = null;
+            $this->precio_venta_evaluacion = null;
+            $this->ley_evaluacion = null;
+            $this->calidad_evaluacion = null;
+            $this->volumen_comercializado_evaluacion = null;
+            $this->volumen_acopiado_evaluacion = null;
+            $this->volumen_descarte_evaluacion = null;
+            $this->capacidad_evaluacion = null;
+            $this->explotacion_evaluacion = null;
+            $this->nombre_mineral_comentario = null;
+            $this->id_mina_evaluacion = null;
+            $this->variedad_comentario = null;
+            $this->produccion_comentario = null;
+            $this->unidades_comentario = null;
+            $this->precio_venta_comentario = null;
+            $this->ley_comentario = null;
+            $this->calidad_comentario = null;
+            $this->volumen_comercializado_comentario = null;
+            $this->volumen_acopiado_comentario = null;
+            $this->volumen_descarte_comentario = null;
+            $this->capacidad_comentario = null;
+            $this->explotacion_comentario = null;
+        } else {
+            $this->nombre_mineral_evaluacion = false;
+            $this->id_mina_evaluacion = false;
+            $this->variedad_evaluacion = false;
+            $this->produccion_evaluacion = false;
+            $this->unidades_evaluacion = false;
+            $this->precio_venta_evaluacion = false;
+            $this->ley_evaluacion = false;
+            $this->calidad_evaluacion = false;
+            $this->volumen_comercializado_evaluacion = false;
+            $this->volumen_acopiado_evaluacion = false;
+            $this->volumen_descarte_evaluacion = false;
+            $this->capacidad_evaluacion = false;
+            $this->explotacion_evaluacion = false;
+            $this->nombre_mineral_comentario = $faker->realText($maxNbChars = 200, $indexSize = 2);
+            $this->id_mina_evaluacion = $faker->realText($maxNbChars = 200, $indexSize = 2);
+            $this->variedad_comentario = $faker->realText($maxNbChars = 200, $indexSize = 2);
+            $this->produccion_comentario = $faker->realText($maxNbChars = 200, $indexSize = 2);
+            $this->unidades_comentario = $faker->realText($maxNbChars = 200, $indexSize = 2);
+            $this->precio_venta_comentario = $faker->realText($maxNbChars = 200, $indexSize = 2);
+            $this->ley_comentario = $faker->realText($maxNbChars = 200, $indexSize = 2);
+            $this->calidad_comentario = $faker->realText($maxNbChars = 200, $indexSize = 2);
+            $this->volumen_comercializado_comentario = $faker->realText($maxNbChars = 200, $indexSize = 2);
+            $this->volumen_acopiado_comentario = $faker->realText($maxNbChars = 200, $indexSize = 2);
+            $this->volumen_descarte_comentario = $faker->realText($maxNbChars = 200, $indexSize = 2);
+            $this->capacidad_comentario = $faker->realText($maxNbChars = 200, $indexSize = 2);
+            $this->explotacion_comentario = $faker->realText($maxNbChars = 200, $indexSize = 2);
+        }
+        $this->save();
         // San Luis
         /*'ley',
         'calidad',
